de25de317c4d5f2ecd3b71ecefd847987530ce50
Piotr Kowalski <piecioshka@gmail.com>
2026-06-09 10:32:47 +0200

    feat: add -h/--help flag

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
+var args = process.argv.slice(2);
+
+if (args.indexOf('-h') !== -1 || args.indexOf('--help') !== -1) {
+    console.log([
+        'Usage: underscore-methods-usage-statistics [directory]',
+        '',
+        '  Display statistics of Underscore.js methods used in a codebase.',
+        '',
+        'Arguments:',
+        '  directory    Directory to scan recursively (default: current directory).',
+        '',
+        'Options:',
+        '  -h, --help   Show this help and exit.',
+        '',
+        'Example:',
+        '  underscore-methods-usage-statistics ./src'
+    ].join('\n'));
+    process.exit(0);
+}
+
 require('../src/analyzer').setup();
